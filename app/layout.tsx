067ff46b6ea63f82e99cd062531e4da7e336df4a
Naman Ahuja <naman6176@gmail.com>
2024-12-31 13:01:18 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import DeployButton from "@/components/deploy-button";
+// import DeployButton from "@/components/deploy-button"; 
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
@@ -14,7 +14,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
+  title: "GetyoLogo",
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
@@ -42,9 +42,9 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
+                    {/* <Link href={"/"}>Next.js Supabase Starter</Link> */}
                     <div className="flex items-center gap-2">
-                      <DeployButton />
+                      {/* <DeployButton /> */}
                     </div>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
@@ -55,7 +55,7 @@ export default function RootLayout({
               </div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
+                {/* <p>
                   Powered by{" "}
                   <a
                     href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
@@ -65,7 +65,8 @@ export default function RootLayout({
                   >
                     Supabase
                   </a>
-                </p>
+                </p> */}
+                
                 <ThemeSwitcher />
               </footer>
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import React from 'react'
 
-export default async function Home() {
+function page() {
   return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
+    <div>
+      <h1>Get a logo for your business </h1>
+    
+
+
+      <Button><Link href="/sign-up">Generate a logo</Link></Button>
+
+      
+    </div>
+  )
 }
+
+export default page
